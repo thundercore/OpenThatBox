7ae241733d88c8aebf3bbf8ab4290f1967e7c26a
Yifan Jiang <yifan.jiang@thundertoken.com>
2019-11-08 16:31:31 -0500

    add in build

---
+++ frontend/build/precache-manifest.3386cc2d15f542a4fb911c1354333e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4554d1e77b47fd8edc8aa75234bc9d",
+    "url": "OpenThatBox/frontend/build/index.html"
+  },
+  {
+    "revision": "2910e6b30f1411b71da6",
+    "url": "OpenThatBox/frontend/build/static/js/2.836f0493.chunk.js"
+  },
+  {
+    "revision": "377aacf3cd1184060b72",
+    "url": "OpenThatBox/frontend/build/static/js/main.2fbdc520.chunk.js"
+  },
+  {
+    "revision": "8c485b22d7700f0657b9",
+    "url": "OpenThatBox/frontend/build/static/js/runtime-main.87206d92.js"
+  },
+  {
+    "revision": "3f0598f386883cfc167bedd5a2055c81",
+    "url": "OpenThatBox/frontend/build/static/media/box-closed.3f0598f3.png"
+  }
+]);
